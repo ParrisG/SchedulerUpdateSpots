4022466e88faa72308dbb83db24094fa5dec08ce
Parris Greaves <parris.greaves@gmail.com>
2021-11-02 23:26:22 -0400

    Figured it out!

--- updateSpots.js
+++ updateSpots.js
@@ -8,9 +8,26 @@
  */
 
 const updateSpots = function (state, appointments, id) {
+  let spotCount = 0;
 
-  // return days array
-  return [];
+  const daysIndex = state.days.findIndex((day) => day.name === state.day);
+  let appointArr = state.days[daysIndex].appointments;
+
+  for (let key of appointArr) {
+    if (appointments[key].interview === null) {
+      spotCount++;
+    }
+  }
+
+  const day = {
+    ...state.days[daysIndex],
+    spots: spotCount
+  };
+  const beginning = state.days.slice(0, daysIndex);
+  const ending = state.days.slice(daysIndex + 1, state.days.length);
+  const days = [...beginning, day, ...ending];
+
+  return days;
 };
 
 module.exports = updateSpots;
